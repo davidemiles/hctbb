9671c44d730cd8aa61de8f46064cbafa0d4bb9f6
davidemiles <me@davidmiles.blog>
2022-08-16 13:44:52 -0400

    Update dummy

--- public/dummy.js
+++ public/dummy.js
@@ -1,20 +1,3 @@
- self.addEventListener("install", function (e) {
-  e.waitUntil(
-    caches.open("hctbb").then(function (cache) {
-      return cache.addAll(["/"]);
-    })
-  );
-});
- 
-self.addEventListener("fetch", function (event) {
-  console.log(event.request.url);
- 
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
-    })
-  );
-});
 if (
   location.protocol !== "https:" &&
   location.hostname != "127.0.0.1" &&
